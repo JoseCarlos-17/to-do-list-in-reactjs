c048d1a3eb6f78a6cc5fabc6620fab821f119212
Jose_Carlos_17 <josecarlosbsilva.172@gmail.com>
2025-03-13 12:36:26 -0300

    add the fields and a min css to fields

--- src/views/TaskFormScreen/TaskFormScreen.jsx
+++ src/views/TaskFormScreen/TaskFormScreen.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react'
+import './TaskFormScreen.css'
+
 
 function TaskFormScreen() {
+  const [task, setTask] = useState({ title: '', description: '' })
+
   return (
     <>
       <h1>Task Form</h1>
+      <form action="">
+        <div className='form-item'>
+          <label>Title: </label>
+          <input
+            type="text"
+            value={task.title}
+            onChange={e => setTask({...task, title: e.target.value})}
+          />
+        </div>
+       
+        <div className='form-item'>
+          <label>Description: </label>
+          <input
+            type="text"
+            value={task.description}
+            onChange={e => setTask({...task, description: e.target.value})}
+          />
+        </div>
+      </form>
     </>
   )
 }
